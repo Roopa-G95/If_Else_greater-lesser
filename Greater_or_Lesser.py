dfbd66465bf046a1c1617157d8f52774bf8e2caa
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-12-02 16:35:10 +0530

    Greaterlesser

---
+++ Greater_or_Lesser.py
@@ -0,0 +1,9 @@
+a=int(input('Enter input1'))
+b=int(input('Enter input2'))
+if(a>b):
+    print('Input1 is greater than input2 and input2 is less than input1')
+elif(a<b):
+    print('Input2 is greater than input1 and input1 is less than input2')
+else:
+    print('Input1=input2')
+
